70fbfaa1f13ae4ddbef0c45d105416613cea0ae0
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-09-05 17:42:05 +0100

    database linked

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require 'sinatra/base'
-
+require './app/models/link.rb'
 class App < Sinatra::Base
+
+  get '/' do
+    redirect '/links'
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb(:links)
+  end
+
+
+
+
+
+
+
+
 
 end
--- spec/features/homepage_links_spec.rb
+++ spec/features/homepage_links_spec.rb
@@ -5,7 +5,6 @@ feature 'viewing links' do
   scenario 'I see links on homepage' do
     Link.create(url:'www.google.com', title: 'Google Homepage')
     visit '/links'
-
     within 'ul#links' do
     expect(page).to have_content 'Google Homepage'
     end
