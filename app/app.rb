e5e4bb1d9b6439e478b5ccf2a6a1d43c74c160b2
Abigail <abigail.mcphillips@gmail.com>
2016-09-06 14:56:48 +0100

    adds homepage

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,11 @@ require_relative 'models/link'
 
 
 class BookmarkManager < Sinatra::Base
+  get '/' do
+    redirect '/link/new'
+  end
+
+
   get '/links' do
     @links = Link.all
     erb :'links/index'
