6d0a60b6a696bee078ed93dbf3afdadb8a1b4298
jameshamann <jameshamann@me.com>
2016-09-06 13:07:01 +0100

    adds different dbs for test and dev enviornments

--- Gemfile
+++ Gemfile
@@ -4,12 +4,11 @@ source "https://rubygems.org"
 
   ruby '2.2.3'
 
+    gem 'sinatra'
+    gem 'rspec-sinatra'
+    gem 'capybara'
+    gem 'database_cleaner'
+
 # gem "rails"
   gem 'data_mapper'
   gem 'dm-postgres-adapter'
-
-group :test do
-  gem 'rspec-sinatra'
-  gem 'capybara'
-  gem 'database_cleaner'
-end
--- app/models/link.rb
+++ app/models/link.rb
@@ -12,9 +12,7 @@ class Link
 
 end
 
-# Now let's set up a connection with a database
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_test")
-# Let's check that everything we wrote in our models was OK
+
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV["RACK_ENV"]}")
 DataMapper.finalize
-# And let's build any new columns or tables we added
 DataMapper.auto_upgrade!
